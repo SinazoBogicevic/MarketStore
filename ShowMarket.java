fc25b8ff524ab6f91ecc06c8a09b77c30bfa6387
Sinazo Bogicevic <snoqhamza@yahoo.com>
2020-07-06 12:01:48 +0200

    Initial sharing of project

---
+++ ShowMarket.java
@@ -0,0 +1,14 @@
+import MarketStore.*;
+/**
+ * Write a description of ShowMarket here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ShowMarket {
+    
+    public static void main(String []args){
+        MarketStore marketStore = new MarketStore();
+        marketStore.showMarketStore();
+    }
+}
